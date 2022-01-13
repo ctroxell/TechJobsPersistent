1a225ab91c10c733c28e4c83702621afa8b46f16
Claire Troxell <cltroxell@gmail.com>
2022-01-13 09:14:29 -0600

    completed part 1

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TechJobsPersistent.Data;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
 
@@ -12,25 +14,54 @@ namespace TechJobsPersistent.Controllers
 {
     public class EmployerController : Controller
     {
+
+        private JobDbContext context;
+
+        public EmployerController(JobDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<Employer> employers = context.Employers
+                .Include(e => e.Name)
+                .ToList();
+
+            return View(employers);
         }
 
         public IActionResult Add()
         {
-            return View();
+            //List<Employer> employers = context.Employers.ToList();
+            AddEmployerViewModel addEmployerViewModel = new AddEmployerViewModel();
+            return View(addEmployerViewModel);
         }
 
-        public IActionResult ProcessAddEmployerForm()
+        public IActionResult ProcessAddEmployerForm(AddEmployerViewModel addEmployerViewModel)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                Employer newEmployer = new Employer
+                {
+                    Name = addEmployerViewModel.Name,
+                    Location = addEmployerViewModel.Location,
+                };
+
+                context.Employers.Add(newEmployer);
+                context.SaveChanges();
+
+                return Redirect("/Employer");
+            }
+
+            return View(addEmployerViewModel);
         }
 
         public IActionResult About(int id)
         {
-            return View();
+            Employer employer = context.Employers.Find(id);
+            return View(employer);
         }
     }
 }
--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,5 +1,6 @@
 --Part 1
-
+SELECT *
+FROM techjobs.jobs
 --Part 2
 
 --Part 3
